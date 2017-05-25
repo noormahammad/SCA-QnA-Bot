ed4d66d813e97bd1ca70a94526189bb61b68730b
Noor <noormahammad@gmail.com>
2017-05-25 14:12:51 -0400

    Add project files.

---
+++ SCA-QNA-Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using QnAMakerDialog;
+
+namespace SCA_QNA_Bot.Dialogs
+{
+    [Serializable]
+    [QnAMakerService("6efef4ee064b4afa9c18fb512f11609a", "710c9068-608b-4b0d-8aaf-90bdf52d2605")]
+    public class RootDialog : QnAMakerDialog<Object>
+    {
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            await context.PostAsync($"Sorry, I could not find an answer for '{originalQueryText}'. ");
+            context.Wait(MessageReceived);
+        }
+
+        [QnAMakerResponseHandler(50)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            await context.PostAsync($"I found an answer that might help...{result.Answer}.");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ SCA-QNA-Bot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SCA_QNA_Bot.WebApiApplication" Language="C#" %>
